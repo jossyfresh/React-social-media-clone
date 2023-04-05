e09dcf5675c3f92e1e0d0009cb7ebf8d512454c3
Yoseph Shemeles <jossyfresh2002@gmail.com>
2023-04-05 03:36:21 +0300

    Setting up project enviroment

---
+++ social-media-clone/src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDSd6Ln8MjUfkyjPeXMl4sk_TAE1qpGQz8",
+  authDomain: "social-media-clone-d9078.firebaseapp.com",
+  projectId: "social-media-clone-d9078",
+  storageBucket: "social-media-clone-d9078.appspot.com",
+  messagingSenderId: "407057180569",
+  appId: "1:407057180569:web:0927d35f56672ad56ad508",
+  measurementId: "G-E0N5810NFD",
+};
+
+const app = initializeApp(firebaseConfig);
